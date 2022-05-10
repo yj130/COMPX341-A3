a68150e74d33090970cb54c4b5c9cef9b6c69243
Yuchen_Jia <jiayuchen130@gmail.com>
2022-05-10 22:41:10 +1200

    Now is the right codes

--- assets/src/modules/bestSellers/bestSellersBar/BestSellersBar.tsx
+++ assets/src/modules/bestSellers/bestSellersBar/BestSellersBar.tsx
@@ -16,7 +16,8 @@ export class BestSellersBar extends React.Component {
   render() {
     return (
       <div className="center ad-gallery nav">
-        
+        <div className="col-md-2 hidden-sm hidden-xs">
+          <LinkContainer to="/best">
             <NavItem><h3>Bookstore<br/>Best Sellers</h3></NavItem>
           </LinkContainer>
         </div>
@@ -32,5 +33,4 @@ export class BestSellersBar extends React.Component {
     );
   }
 }
-
 export default BestSellersBar;
