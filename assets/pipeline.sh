9fa213d807dda0252216b7100550434ffa043028
Yuchen_Jia <jiayuchen130@gmail.com>
2022-05-12 10:52:19 +1200

    This i a test commit

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #npm install
-#npm run build || exit 0
+#npm run build || echo "Complied-time Errors" && exit 0
 
 #echo 'Complied-time passed!'
 git add .
 
 echo 'Please provide a commit message(Press 'Enter' to confirm)'
 read commit
- [ -z "$commit" ] && echo "Your commit is empty!" || git commit -m "$commit"
+ [ -z "$commit" ] && echo "Your commit is empty!" && exit 0 || git commit -m "$commit"
 #git push
 
 #npm run start 
