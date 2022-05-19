764c031e94604bdafe416b37538393e1fe5e5083
Yuchen_Jia <jiayuchen130@gmail.com>
2022-05-20 10:52:08 +1200

    change some errors

--- assets/auto_mod.sh
+++ assets/auto_mod.sh
@@ -1,6 +1,2 @@
 #!/bin/sh  
-<<<<<<< HEAD
 find . -name '*.ts' | xargs sed -i '1i\//Name:Yuchen Jia  ID:1538001'
-=======
-find . -name '*.ts' | xargs sed -i '1i\//Name:Yuchen Jia  ID:1538001'
->>>>>>> 3e47ae3b63207c3e011a743c40f792c385cf2b98
--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -7,9 +7,3 @@ echo 'Please provide a commit message(Press 'Enter' to confirm)'
 read commit
  [ -z "$commit" ] && echo "Your commit is empty!" && exit 0 || git commit -m "$commit"
 git push
-<<<<<<< HEAD
-npm run start 
-=======
-
-npm run start 
->>>>>>> 3e47ae3b63207c3e011a743c40f792c385cf2b98
